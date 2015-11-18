eb521ea9ff1c0c334201ca5e9e2ca07f682a67e4
somesayss <413461856@qq.com>
2015-11-18 09:27:01 +0800

    add

--- src/js/modules/reactForm/textarea.js
+++ src/js/modules/reactForm/textarea.js
@@ -21,8 +21,7 @@ define(function(require, exports, module) {
 			var me = this,
 				node = me.node;
 			node.height( me.props.height || 16 );
-			console.log(node.prop('scrollHeight'), me.padHeight);
-			// node.height(node.prop('scrollHeight') - me.padHeight);
+			node.height( ( Math.max( node.prop('scrollHeight'), node.prop('clientHeight')) ) - me.padHeight );
 		},
 		render: function(){
 			var me = this;
