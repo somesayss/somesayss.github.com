a80afe15f442bc53439b341483439017e68fc351
somesayss <413461856@qq.com>
2015-11-18 09:50:00 +0800

    add

--- src/js/bus/react/main.js
+++ src/js/bus/react/main.js
@@ -20,7 +20,7 @@ define(function(require, exports, module) {
 				diqu: "zhejiang",
 				sex: "female",
 				like: ["zhuqiu", "paiqiu"],
-				jianjie: "我是简介"
+				jianjie: "我是简介1"
 			}
 		},
 		change: function(state){
