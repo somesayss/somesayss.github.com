e9aec01e846fe651e04b8d2785a5b16b61b8f86c
somesayss <413461856@qq.com>
2017-08-14 17:41:57 +0800

    add

--- do.sh
+++ do.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo -e '1.GIT打包上传[git]\n2.创建页面[page]\n3.创建模块[mod]'
-echo -n '输入你想要的操作:'
+echo -e "1.GIT打包上传[git]\n2.创建页面[page]\n3.创建模块[mod]"
+echo -n "输入你想要的操作:"
 read type
 
 gitDO(){
+
+	echo -n "输入注释:"
+	read commit
+	[ ! $commit ] && commit="add"
+	echo -n "输入分支(默认是master):"
+	read master
+	[ ! $master ] && master="master"
 	git add -A
-	git commit -m 'add'
+	git commit -m $commit
+	git push origin $master;
 }
 
 ${type}DO
