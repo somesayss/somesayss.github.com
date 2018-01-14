6f0d706d1e6b79cbdde65e75f4abca0dd59d8335
somesayss <413461856@qq.com>
2018-01-14 15:01:58 +0800

    add

--- config/staticConfig.js
+++ config/staticConfig.js
@@ -3,11 +3,7 @@ module.exports = {
 	commonJsList: [
 		'src/js/libs/jquery.js',
 	    'src/js/libs/react.js',
-	    'src/bower_components/limit/dist/limit.min.js'
-	],
-	commonCssList: [
-		'src/bower_components/limitCss/dist/reset.min.css',
-    	'src/bower_components/limitCss/dist/global.min.css'
+	    'src/js/libs/limit.min.js'
 	],
 	staticList: [
 		'imgs',
